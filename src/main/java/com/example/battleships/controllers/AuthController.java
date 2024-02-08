4695834263ab7151d3dcbb0318861e768e39ec5c
Snezhana Stateva <114188863+Stateva91@users.noreply.github.com>
2024-02-08 21:19:17 +0100

    Battleships Java Spring Project

--- src/main/java/com/example/battleships/controllers/AuthController.java
+++ src/main/java/com/example/battleships/controllers/AuthController.java
@@ -1,10 +1,12 @@
 package com.example.battleships.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AuthController {
 
+    @GetMapping("/register")
     public String register(){
         return "register";
     }
