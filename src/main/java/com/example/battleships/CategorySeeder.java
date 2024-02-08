75f3923a6d75eaf17d58116487f157ecfc105c64
Snezhana Stateva <114188863+Stateva91@users.noreply.github.com>
2024-02-08 20:36:11 +0100

    Battleships Java Spring Project

--- src/main/java/com/example/battleships/CategorySeeder.java
+++ src/main/java/com/example/battleships/CategorySeeder.java
@@ -1,9 +1,16 @@
 package com.example.battleships;
 
+import com.example.battleships.models.Category;
+import com.example.battleships.models.Ship;
+import com.example.battleships.models.ShipType;
 import com.example.battleships.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class CategorySeeder implements CommandLineRunner {
@@ -18,7 +25,10 @@ public class CategorySeeder implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
      if(this.categoryRepository.count()==0){
-         System.out.println("seed");
+         List<Category> categories = Arrays.stream(ShipType.values())
+                 .map(Category::new)
+                 .collect(Collectors.toList());
+      this.categoryRepository.saveAll(categories);
      }
     }
 }
